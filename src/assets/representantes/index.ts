a2f5d8fc930973e5bc227e872c0d331cade22391
João Lenon <lenonsec7@gmail.com>
2020-08-31 11:15:45 -0300

    feat: First Boilerplate

---
+++ src/assets/representantes/index.ts
@@ -0,0 +1,7 @@
+import agvLogo from './agvLogo.svg'
+import headsightLogo from './headsightLogo.svg'
+import precisionLogo from './precisionLogo.svg'
+import shredLogo from './shredLogo.svg'
+import trimbleLogo from './trimbleLogo.svg'
+
+export { agvLogo, headsightLogo, precisionLogo, shredLogo, trimbleLogo }
