06b959958cf50b14a7b84dd893315ae80bc82a1b
João Lenon <lenonsec7@gmail.com>
2020-08-31 11:16:03 -0300

    feat: First Boilerplate

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const FooterS = styled.footer`
+  padding: 32px 0;
+  background: #28262e;
+`
+
+export const FooterContent = styled.div`
+  > img {
+    margin-top: -70px;
+
+    width: 250px;
+  }
+
+  max-width: 1120px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    margin-top: -30px;
+    font-size: 20px;
+  }
+`
